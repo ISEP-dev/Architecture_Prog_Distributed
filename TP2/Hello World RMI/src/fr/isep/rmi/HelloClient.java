99f5deff687142d4f63b194abb307c7a87d30e69
bastiengrignon <basgrignon@gmail.com>
2020-10-14 23:44:54 +0200

    REFACTOR HelloClient method

--- TP2/Hello World RMI/src/fr/isep/rmi/HelloClient.java
+++ TP2/Hello World RMI/src/fr/isep/rmi/HelloClient.java
@@ -3,21 +3,17 @@ package fr.isep.rmi;
 import java.rmi.*;
 
 public class HelloClient {
-	public static void main(String[] args)
-	{
-		try 
-		{
-      // lookup for the object
-			HelloInterface obj = (HelloInterface)	Naming.lookup("rmi://localhost:12345/mon_serveur_hello");
-			
-			// call methods
-			String msg = obj.say();
-			System.out.println(msg);
-			
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            // lookup for the object
+            HelloInterface obj = (HelloInterface) Naming.lookup("rmi://localhost:12345/mon_serveur_hello");
+
+            // call methods
+            String msg = obj.say();
+            System.out.println(msg);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

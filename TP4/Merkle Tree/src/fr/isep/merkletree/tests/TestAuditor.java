c5596924577da1f8105a4e73a90b68615b4cbe00
bastiengrignon <basgrignon@gmail.com>
2020-12-14 14:10:14 +0100

    ADD tests

---
+++ TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestAuditor.java
@@ -0,0 +1,7 @@
+package fr.isep.merkletree.tests;
+
+public class TestAuditor {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestHash.java
@@ -0,0 +1,22 @@
+package fr.isep.merkletree.tests;
+
+import fr.isep.merkletree.Hash;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestHash {
+    @Test
+    public void testHash() {
+        String s1 = "1";
+        String s2 = "1";
+
+        Hash hash1 = new Hash(s1);
+        Hash hash2 = new Hash(s2);
+        System.out.println("hash1: " + hash1.toString() + " -> " + hash1.hashLength);
+        System.out.println("hash2: " + hash2.toString() + " -> " + hash2.hashLength);
+
+        assertEquals(hash1.hashLength, 32);
+        assertEquals(hash2.hashLength, 32);
+    }
+}

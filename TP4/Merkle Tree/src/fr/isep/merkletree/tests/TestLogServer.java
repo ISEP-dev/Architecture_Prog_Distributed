1e7dad1102eae78e4fb534890f3c53950a36bc5f
bastiengrignon <basgrignon@gmail.com>
2020-12-14 17:15:03 +0100

    Add tests logServer

--- TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestLogServer.java
+++ TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestLogServer.java
@@ -1,12 +1,14 @@
 package fr.isep.merkletree.tests;
 
+import fr.isep.merkletree.Hash;
 import fr.isep.merkletree.LogServer;
 import fr.isep.merkletree.MerkleTree;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 
 public class TestLogServer {
     @Test
@@ -24,10 +26,40 @@ public class TestLogServer {
     @Test
     public void testLogServerFile() {
         LogServer logServer = new LogServer("tests/testFile.txt");
-        MerkleTree merkleTree = new MerkleTree("Warning", 7);
-        LogServer logServerMerkle = new LogServer(merkleTree);
+        MerkleTree merkleTreeLeft = new MerkleTree("test1", 4);
+        MerkleTree merkleTreeRight = new MerkleTree("test2", 5);
+        MerkleTree serverMerkleTree = new MerkleTree(merkleTreeLeft, merkleTreeRight);
 
-        logServer.getTree().displayTree(0);
-        logServerMerkle.getTree().displayTree(0);
+        logServer.getTree().displayTree(4);
+        serverMerkleTree.displayTree(5);
+    }
+
+    @Test
+    public void testGenPath() {
+        LogServer logServer = new LogServer("tests/testFile.txt");
+
+        MerkleTree merkleTreeLeft = new MerkleTree("test1", 4);
+        MerkleTree merkleTreeRight = new MerkleTree("test2", 5);
+        MerkleTree merkleTreeExpected = new MerkleTree(merkleTreeLeft, merkleTreeRight);
+
+        LinkedList<Hash> path = logServer.genPath(0);
+        LinkedList<Hash> expectedPath = new LinkedList<>(Collections.singletonList(merkleTreeExpected.getRight().getHash()));
+
+        assertEquals(expectedPath.toArray().length, path.toArray().length);
+    }
+
+    @Test
+    public void testAppendOne() {
+        LogServer logServer = new LogServer("tests/testFile.txt");
+        logServer.append("test3");
+
+        MerkleTree mLeft = new MerkleTree("test1", 4);
+        MerkleTree mRight = new MerkleTree("test2", 5);
+        MerkleTree merkleTreeMerge = new MerkleTree(mLeft, mRight);
+
+        MerkleTree mRight2 = new MerkleTree("test3", 6);
+        MerkleTree mAppend = new MerkleTree(merkleTreeMerge, mRight2);
+
+        assertEquals(mAppend.getHash(), logServer.getTree().getHash());
     }
 }

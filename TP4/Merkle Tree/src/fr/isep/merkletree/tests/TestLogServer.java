e7292b118eecd4a1f2208f3db53f57b00a51becc
bastiengrignon <basgrignon@gmail.com>
2020-12-14 16:00:19 +0100

    ADD test ServerLog

--- TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestLogServer.java
+++ TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestLogServer.java
@@ -1,10 +1,33 @@
 package fr.isep.merkletree.tests;
 
+import fr.isep.merkletree.LogServer;
+import fr.isep.merkletree.MerkleTree;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public class TestLogServer {
     @Test
     public void testLogServer() {
+        MerkleTree merkleTree = new MerkleTree("testing", 1);
 
+        LogServer logServer = new LogServer(merkleTree);
+
+        logServer.getTree().displayTree(0);
+
+        assertEquals(logServer.getTree(), merkleTree);
+        assertEquals(logServer.currentRootHash(), merkleTree.getHash());
+    }
+
+    @Test
+    public void testLogServerFile() {
+        LogServer logServer = new LogServer("tests/testFile.txt");
+        MerkleTree merkleTree = new MerkleTree("Warning", 7);
+        LogServer logServerMerkle = new LogServer(merkleTree);
+
+        logServer.getTree().displayTree(0);
+        logServerMerkle.getTree().displayTree(0);
     }
 }

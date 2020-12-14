fe2a4f876ccc9fbcf308c7a08855408808d5663a
bastiengrignon <basgrignon@gmail.com>
2020-12-14 17:15:11 +0100

    Add Auditor tests

--- TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestAuditor.java
+++ TP4/Merkle Tree/src/fr/isep/merkletree/tests/TestAuditor.java
@@ -1,10 +1,28 @@
 package fr.isep.merkletree.tests;
 
+import fr.isep.merkletree.Auditor;
+import fr.isep.merkletree.Hash;
+import fr.isep.merkletree.LogServer;
 import org.junit.Test;
 
-public class TestAuditor {
-    @Test
-    public void testAuditor() {
+import java.util.LinkedList;
 
+import static org.junit.Assert.assertEquals;
+
+public class TestAuditor {
+    public void AuditorConstructor() {
+        LogServer logServer = new LogServer("tests/testFile.txt");
+        Auditor auditor = new Auditor(logServer);
+
+        assertEquals(auditor.getLogServer().getTree().getHash(), logServer.getTree().getHash());
+    }
+
+    @Test()
+    public void buildHashEndNull() {
+        LogServer l = new LogServer("tests/testFile.txt");
+        Auditor a = new Auditor(l);
+
+        Hash hash  = a.buildNewHash("test", 0, 0, new LinkedList<>());
+        assertEquals(new Hash("test"), hash);
     }
 }
